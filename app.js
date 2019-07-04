7d797747ea0d9f6da2b3d1f1e34378b9f05ecb12
lakshitapillay <pillaylakshita@gmail.com>
2019-07-04 21:36:35 +0530

    auth handled

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/' ,(req, res) => {
     res.send('it works');
 });
+
+app.use('/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://lakshita:Lelelelel1!@cluster0-16uxs.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID : '191397948200-063vvl1kptdc4uq26gs6sn9aphuog5o8.apps.googleusercontent.com',
+    googleClientSecret: 'KjjYicnppemLj7DMrfZYSjij'
+}
